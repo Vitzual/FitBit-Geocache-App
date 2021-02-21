350449da7cede99a01d012a73a7a3d1f06317d74
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-02-21 14:59:55 -0800

    Add google login request

--- app/src/main/java/com/example/fitbitgroupk/MainActivity.java
+++ app/src/main/java/com/example/fitbitgroupk/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.fitbitgroupk;
 
+// Default libraries
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+// Google sign in library shit
+import com.google.android.gms.auth.api.signin.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare each members buttons
+    private Button loginButton;
+    private GoogleSignInClient gsi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a new google sign in object (GSI)
+        // This is where we'd add Google Fit permission requests
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        gsi = GoogleSignIn.getClient(this, gso);
+
+        // Set the loginButton
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                permissionCheck();
+            }
+        });
+    }
+
+    // Permission check method
+    public void permissionCheck() {
+
+        // Generate a sign in request with designated intents
+        Intent signInIntent = gsi.getSignInIntent();
+        startActivityForResult(signInIntent, 12345);
+
+        // do shit with it (still unsure about this part)
+
     }
 }
