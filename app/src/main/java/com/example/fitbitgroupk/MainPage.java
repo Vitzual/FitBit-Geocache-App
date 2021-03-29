d006d7fde0a3846828d851cd2d90e84cab5ebf06
Robert <rob.stewart.79@gmail.com>
2021-03-29 11:10:11 -0700

    Bundled the accoutn object from mainActivity and passed it to the mainpage.java and tested that we can access the account object

--- app/src/main/java/com/example/fitbitgroupk/MainPage.java
+++ app/src/main/java/com/example/fitbitgroupk/MainPage.java
@@ -1,14 +1,35 @@
 package com.example.fitbitgroupk;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 public class MainPage extends AppCompatActivity {
 
+    private String TAG = "MAIN_PAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        GoogleSignInAccount account =getIntent().getParcelableExtra("ACCOUNT");
+        String personGivenName = account.getGivenName();
+        String personFamilyName = account.getFamilyName();
+        String personEmail = account.getEmail();
+        String personId = account.getId();
+        Uri personPhoto = account.getPhotoUrl();
+
+
+        //
+        Log.e(TAG, "MainPage Running");
+        Log.e(TAG, personFamilyName.toString());
+        Log.e(TAG, personGivenName.toString());
+        Log.e(TAG, personEmail.toString());
+        Log.e(TAG, personId.toString());
+
     }
 }
